a9cecf126040845c97be97faa44174b62accbf7c
Duda Sena <duda.sruiz@gmail.com>
2026-03-24 10:59:30 -0300

    feat: backend ok

---
+++ backend/src/main/java/com/focusstudy/backend/model/entity/Usuario.java
@@ -0,0 +1,36 @@
+package com.focusstudy.backend.model;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "usuarios")
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    @Setter
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "papeis_usuario",
+            joinColumns = @JoinColumn(name = "usuario_id"),
+            inverseJoinColumns = @JoinColumn(name = "papel_id")
+    )
+    private List<Papel> papeis;
+
+}
